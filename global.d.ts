13bc2b11741e1b9bb020bacc0b90de7fee159c15
sajjadheydari1994 <heydari@hero.best>
2025-12-14 20:06:53 +0330

    feat: install needed dependencies

---
+++ global.d.ts
@@ -0,0 +1,13 @@
+// Allow importing CSS files in TS/TSX (global and module CSS)
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+
+// For imported images (optional convenience)
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+
+export {};
